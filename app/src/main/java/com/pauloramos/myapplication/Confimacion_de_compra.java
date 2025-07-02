629ea24ad93212139e8cf9b9479b75e6d64eccb5
Paulo Ramos <anuelitoramos@gmail.com>
2025-07-01 23:59:24 -0500

    inicio

---
+++ app/src/main/java/com/pauloramos/myapplication/Confimacion_de_compra.java
@@ -0,0 +1,14 @@
+package com.pauloramos.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Confimacion_de_compra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confimacion_de_compra);
+    }
+}
